9dabcf4d40dcd6473db7a617c2f7aacda601392f
Jongmoo Park <stylack@gmail.com>
2024-10-11 13:16:50 +0900

    Flyweight 패턴 실습 완료

---
+++ Flyweight/MainEntry.java
@@ -0,0 +1,12 @@
+package Flyweight;
+
+public class MainEntry {
+  public static void main(String[] args) {
+    Number number = new Number(7434331);
+    number.print(5, 5);
+
+    System.out.println();
+    System.out.println();
+    System.out.println();
+  }
+}
