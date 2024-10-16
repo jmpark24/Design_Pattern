f83431009bebc0ccb458f62c56e2839aae368d30
Jongmoo Park <stylack@gmail.com>
2024-10-16 14:31:36 +0900

    Visitor 패턴 실습 완료

---
+++ Visitor/AvgVisitor.java
@@ -0,0 +1,19 @@
+package Visitor;
+
+public class AvgVisitor implements Visitor {
+  private int sum = 0, count = 0;
+
+  public double getValue() {
+    return sum / count;
+  }
+
+  @Override
+  public void visit(Unit unit) {
+    if (unit instanceof Item) {
+      sum += ((Item) unit).getValue();
+      count++;
+    } else {
+      unit.accept(this);
+    }
+  }
+}
